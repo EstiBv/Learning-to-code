0e415fda3611a0f3e398dd04f0de29acda83287c
Estibaliz Barato <estibalizbarato@gmail.com>
2020-10-13 17:46:10 +0200

    initial commit

--- module 2/Respaso intermedio/js/main.js
+++ module 2/Respaso intermedio/js/main.js
@@ -7,7 +7,7 @@ function upDateClick(){
   // verificar pasos con console.log
   console.log("compruebo todo ok")
   /* instrucciones:
-  estás siendo escuchado(estás dentro de la funcion que es un evento llamadado upDateClick), así que cuando escuches click en update y escuches click seleccionado en los valores de las caras, imprime el valor de la cara que está siendo seleccionada */
+  estás siendo escuchado(estás dentro de la función que es un evento llamadado upDateClick), así que cuando escuches click en update y escuches click seleccionado en los valores de las caras, imprime el valor de la cara que está siendo seleccionada */
   const click = document.querySelector (".js-select");
   const faceValue = click.value;
   face.innerHTML = faceValue;
@@ -18,15 +18,18 @@ btn.addEventListener ("click",upDateClick);
 
 // segunda función
 function numberRandom (){
-  /* parse in cuando en la biblio de la acción maths vemos que lo usa al aplicar la función con numeros decimales*/
+  /* parseIn cuando en la biblio de la acción maths vemos que lo usa al aplicar la función con numeros decimales*/
   console.log (parseInt (Math.numberRandom));
   if (numberRandom % 2 === 0){
     console.log ("par");
     const background = document.querySelector (".js-container");
-    // background.classList.add(#ffcc00);
+    background.classList.add("container");
   }
     else {
    console.log ("impar");
-   background.classList.remove(#ff9900);
+   const background = document.querySelector (".js-container");
+   background.classList.remove("container");
+   background.classList.add("changecolor");
   } 
 }
+numberRandom();
--- module 2/leson 3/exercise 3/main.js
+++ module 2/leson 3/exercise 3/main.js
@@ -1,10 +1,36 @@
 "use strict";
-let userName = "Alicia";
+// EJERCICIO 2
+// const userName = "Alicia";
 
-if (userName === "Alicia") {
-  console.log("welcome to paradise");
-} else if (userName === "Estíbaliz") {
-  console.log("welcome to paradise again");
-} else {
-  console.log("Sorry, you will go to the hell");
+// if (userName === "Alicia") {
+//   console.log("welcome to paradise");
+// } else if (userName === "Estíbaliz") {
+//   console.log("welcome to paradise again");
+// } else {
+//   console.log("Sorry, you will go to the hell");
+// }
+// EJERCICIO 3 y 4 RESUELTO EN PAIR (AliciaColom)
+
+// EJERCICIO 5
+const container = document.querySelector(".container");
+const message = document.querySelector(".title");
+const textAlert = document.querySelector(".textArea");
+
+if (container.classList.contains(".warning")) {
+  message.innerHTML = 'AVISO';
+  textAlert.innerHTML = 'Tenga cuidado';
+  container.classList.add("warning");
+  
+} else if (container.classList.contains(".success")){
+  message.innerHTML = 'CORRECTO';
+  textAlert.innerHTML = 'Los datos son correctos';
+  container.classList.remove("warning");
+  container.classList.add("success");
 }
+ else (container.classList.contains(".error"))
+  message.innerHTML ='ERROR'
+  textAlert.innerHTML = 'Ha surgido un error'
+  container.classList.remove("success");
+  container.classList.add("error");
+
+ 
