aec0338d5081be28d338ba216c10feff6cbde1ed
Estibaliz Barato <estibalizbarato@gmail.com>
2020-10-17 10:46:25 +0200

    refresh objets

--- module 2/leson 10/ejercicio 3/js/main.js
+++ module 2/leson 10/ejercicio 3/js/main.js
@@ -1,21 +1,28 @@
 // Ejercicio 3
 
 const btn = document.querySelector(".js-btn");
-function nombreFuncion() {
-  const input = document.querySelector(".js-input").value;
+function infoPressButton() {
   /* 1- Se ejecute al pulsar el botón
-  2- Contiente un apetición a la API para obtener info de ese usuario */
+  /* 2-.value recoge el valor del input, y esta acción la recoge ésta segunda constante. Ese valor tiene que estár en la url por eso se añade */
+  const inputUserValue = document.querySelector(".js-input").value;
 
-  fetch(`https://api.github.com/users/${input}`)
+  /* 3- Contiente un apetición a la API para obtener info de ese usuario */
+
+  fetch(`https://api.github.com/users/${inputUserValue}`)
     .then((response) => response.json())
     .then((data) => {
       /* esto es una lista y puedo meterlo en un array, para recorrerlo, y alañadir datos a las clases del html */
+      // const listDataUser = document.querySelectorAll("js-list");
+      // for (let i = 0; i < listDataUser.length; i++) {
+      //   console.log("jola");
+      // }
+
       const dateName = document.querySelector(".name");
-      dateName.innerHTML += data.login;
+      dateName.innerHTML += ":" + " " + data.login;
       console.log(dateName);
 
       const dateRepo = document.querySelector(".repo");
-      dateRepo.innerHTML += data.public_repos;
+      dateRepo.innerHTML += ":" + " " + data.public_repos;
       console.log(dateRepo);
 
       const dateImg = document.querySelector(".js-img");
@@ -27,7 +34,7 @@ function nombreFuncion() {
   // })
 }
 
-btn.addEventListener("click", nombreFuncion);
+btn.addEventListener("click", infoPressButton);
 
 // pintar
 // userGitHub () => {
