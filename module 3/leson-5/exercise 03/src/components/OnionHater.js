d7a72b04028dd175f65ee4d8b310e2ca70f87796
Estibaliz Barato <estibalizbarato@gmail.com>
2020-11-09 11:38:16 +0100

    add exercise 3 leson 5 module 3

--- module 3/leson-5/exercise 03/src/components/OnionHater.js
+++ module 3/leson-5/exercise 03/src/components/OnionHater.js
@@ -1,14 +1,29 @@
 import React from "react";
 import "./App.css";
 
-const handleOnion = (ev) => {
-  if (ev.target.value.includes("cebolla")) {
-    alert("CEEBOOOOLLA PECADORALLR!");
-  }
-};
 class OnionHater extends React.Component {
+  constructor() {
+    super();
+    this.handleOnion = this.handleOnion.bind(this);
+    this.isHating = "";
+  }
+
+  handleOnion(ev) {
+    if (ev.target.value.includes("cebolla")) {
+      this.isHating = this.forceUpdate();
+      this.isHating = "areaBody";
+    } else {
+      this.isHating = this.forceUpdate();
+      this.isHating = "";
+    }
+  }
+
   render() {
-    return <textarea onChange={handleOnion}></textarea>;
+    return (
+      <main className={this.isHating}>
+        <textarea onChange={this.handleOnion}></textarea>
+      </main>
+    );
   }
 }
 
