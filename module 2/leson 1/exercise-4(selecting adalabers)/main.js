8c2094e93be4e326d6f5ac346a5921b3fae736aa
Estibaliz Barato <estibalizbarato@gmail.com>
2021-01-21 17:05:47 +0100

    seleccting Adalabers

---
+++ module 2/leson 1/exercise-4(selecting adalabers)/main.js
@@ -0,0 +1,8 @@
+"use strict";
+const titleResultSelecction = document.querySelector("h3");
+const option3 = document.querySelector(".option3");
+
+titleResultSelecction.innerHTML = titleResultSelecction.innerHTML + ` Estitxu`;
+
+// titleResultSelecction.innerHTML =
+//   titleResultSelecction.innerHTML + ` Falopia Moroño`;
