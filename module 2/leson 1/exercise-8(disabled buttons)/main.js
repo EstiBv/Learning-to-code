b2bde75c4f0976d8eabefc4c89434a9fe7f0437a
Estibaliz Barato <estibalizbarato@gmail.com>
2021-01-21 17:54:48 +0100

    disabled buttons

---
+++ module 2/leson 1/exercise-8(disabled buttons)/main.js
@@ -0,0 +1,6 @@
+"use strict";
+
+const selectButtonOne = document.querySelector(".js-btn1");
+const selectButtonTwo = document.querySelector(".js-btn2");
+
+selectButtonTwo.classList.add("disabled");
