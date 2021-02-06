33b3a3fa831e49e99d39b5b0f8de3aba98d4c48b
Estibaliz Barato <estibalizbarato@gmail.com>
2021-02-06 19:19:50 +0100

    add exercise 05 leson 11

--- module 2/leson 11/exercise-05(favoutireTheme)/main.js
+++ module 2/leson 11/exercise-05(favoutireTheme)/main.js
@@ -14,7 +14,27 @@ const changeTheme = (ev) => {
     mainPage.classList.remove("dark");
     mainPage.classList.add("light");
   }
+  const saveDataTheme = () => {
+    if (listenSelector === "dark") {
+      localStorage.setItem("dark", listenSelector);
+    } else if (listenSelector === "light") {
+      localStorage.removeItem("dark");
+      localStorage.setItem("light", listenSelector);
+    }
+  };
+  saveDataTheme();
 };
+
+const getDataTheme = () => {
+  if (localStorage.getItem("dark")) {
+    mainPage.classList.remove("light");
+    mainPage.classList.add("dark");
+  } else if (localStorage.getItem("light")) {
+    mainPage.classList.remove("dark");
+    mainPage.classList.add("light");
+  }
+};
+getDataTheme();
 
 for (const selector of selectorTheme) {
   selector.addEventListener("click", changeTheme);
